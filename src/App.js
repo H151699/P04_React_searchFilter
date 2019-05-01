bfaecddca2837745125f02546364e354ef7a2e9a
h151699 <h151699@stud.hvl.no>
2019-05-01 13:13:41 +0200

    get json data though map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
+
+const movieDB = [
+
+  {
+      "@id": "7391",
+      "@uri": "/api/contentpanel/element/7391.json",
+      "sortIndex": 0,
+      "title": "Action"
+  },
+  {
+      "@id": "7392",
+      "@uri": "/api/contentpanel/element/7392.json",
+      "sortIndex": 1,
+      "title": "Drama"
+  },
+  {
+      "@id": "7393",
+      "@uri": "/api/contentpanel/element/7393.json",
+      "sortIndex": 2,
+      "title": "Komedie"
+  },
+  {
+      "@id": "7394",
+      "@uri": "/api/contentpanel/element/7394.json",
+      "sortIndex": 3,
+      "title": "Thriller"
+  }
+]
+
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+      this.state ={
+        movies: movieDB
+      
+    }
+  }
+ 
+  render(){ 
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <form><input type="text"
+                  onChange={this.searchHandler} />
+    
+    
+    </form>
+
+           {this.state.movies.map((movie=>           
+           <div>
+              <h3>@id: {movie["@id"]}</h3>
+              <h3>Title: {movie.title}</h3>
+              <h3>SortIndex :  { movie.sortIndex}</h3>
+            </div>
+          )
+           
+           
+           )
+
+        
+}
+</div>
+
+
+
+//     <div className="App">
+//            {movieDB.map(function(movie){
+
+//         return(
+//           <div>
+//             <h3>@id: {movie["@id"]}</h3>
+//             <h3>Title: {movie.title}</h3>
+//             <h3>SortIndex :  { movie.sortIndex}</h3>
+//           </div>
+//         )
+// })}
+// </div>
   );
 }
-
+} 
 export default App;
